effc2b6d509c04b5184b2d0a31a37a305a59df53
ilhamnusantara <ilhamsdr@gmail.com>
2019-12-11 23:09:26 +0700

    gfgf

--- TUGAS TA/App/src/Controller/Controller.java
+++ TUGAS TA/App/src/Controller/Controller.java
@@ -10,5 +10,17 @@ package Controller;
  * @author ilham-07010
  */
 public class Controller {
-    
+    public static void main(String[] args){
+        String text = "Belajar Pemrograman Java";
+        
+        String[] kata = text.split("");
+        
+        String text1 = kata[0];
+        String text2 = kata[1];
+        String text3 = kata[2];
+        
+        System.out.println("Text 1 = "+text1);
+        System.out.println("Text 2 = "+text2);
+        System.out.println("Text 3 = "+text3);
+    }
 }
